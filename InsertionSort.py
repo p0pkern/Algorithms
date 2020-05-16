78683fd9cac1b765b049545567020268eae347a5
p0pkern <p0pkerncs@gmail.com>
2020-05-16 13:39:49 -0700

    Completed functioning insertion sort algorithm.

---
+++ InsertionSort.py
@@ -0,0 +1,16 @@
+def insertion_sort(n):
+    """
+    Basic insertion sort algorithm.
+    :param n: List to be sorted.
+    :return: Sorted list from least to greatest.
+    """
+    for i in range(1, len(n)):
+        key = n[i]
+        j = i - 1
+        print(key, j)
+        while j >= 0 and n[j] > key:
+            n[j + 1] = n[j]
+            j = j - 1
+        n[j + 1] = key
+    return n
+
---
+++ README.md
@@ -0,0 +1,5 @@
+Algorithms
+Author: Chris Peterman
+
+References: 
+1. Introduction to Algorithms - THOMAS H. CORMEN CHARLES E. LEISERSON RONALD L. RIVEST CLIFFORD STEIN
